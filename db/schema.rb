0ba8ca21c19327d7f61669a42e6e59d3bf517332
Aleksa Stevic <aleksa.stevic@fatcatcoders.com>
2024-10-10 20:15:10 +0200

    migrate from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_09_18_094647) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id"
-    t.integer "post_id", null: false
+    t.bigint "user_id"
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -26,7 +29,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_18_094647) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
