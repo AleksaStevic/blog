8553308e110d45c183856e4c31e215afddff9b92
Aleksa Stevic <aleksa.stevic@fatcatcoders.com>
2024-10-10 22:05:51 +0200

    add model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:aleksa)
+  end
+
+  test "should not save post without title" do
+    post = Post.new body: "This is a body", user: @user
+    assert_not post.save, "Saved the post without a title"
+  end
+
+  test "should not save post without body" do
+    post = Post.new title: "This is a title", user: @user
+    assert_not post.save, "Saved the post without a body"
+  end
+
+  test "should not save post without user" do
+    post = Post.new title: "This is a title", body: "This is a body"
+    assert_not post.save, "Saved the post without a user"
+  end
+
+  test "should save post with all attributes" do
+    post = Post.new title: "This is a title", body: "This is a body", user: @user
+    assert post.save, "Could not save the post with a title and body"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without name" do
+    user = User.new email: "aleksa3@fcc.com", password: "secret123"
+    assert_not user.save, "Saved the user without a name"
+  end
+
+  test "should not save user without email" do
+    user = User.new username: "aleksa4", password: "secret123"
+    assert_not user.save, "Saved the user without an email"
+  end
+
+  test "should not save user without password" do
+    user = User.new username: "aleksa5", email: "aleksa5@fcc.com"
+    assert_not user.save, "Saved the user without a password"
+  end
 end
