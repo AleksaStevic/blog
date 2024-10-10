5cd0d699e8075f6bf03e79941b3c233d6bc0268c
Aleksa Stevic <aleksa.stevic@fatcatcoders.com>
2024-10-10 21:13:00 +0200

    cleanup

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-// import CommentController from "./comment_controller";
 
-// application.register("comment", CommentController)
 eagerLoadControllersFrom("controllers", application)
